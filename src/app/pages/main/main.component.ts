783bb609763374fba1196ff72f18b4717a095745
Angelo Brito <angelo@hexacore.com.br>
2025-07-14 22:38:42 -0300

    add background image and test menu on desktop

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -9,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class MainComponent {
   menuOpen = false;
+  isDesktop = false;
+
+  ngOnInit() {
+    this.checkDesktop();
+    window.addEventListener('resize', this.checkDesktop.bind(this));
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('resize', this.checkDesktop.bind(this));
+  }
+
+  checkDesktop() {
+    this.isDesktop = window.innerWidth > 800;
+    if (this.isDesktop) {
+      this.menuOpen = false;
+    }
+  }
+
+  closeMenuIfMobile() {
+    if (!this.isDesktop) {
+      this.menuOpen = false;
+    }
+  }
 }
